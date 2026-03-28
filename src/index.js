ed987898826079086a1dce3774df5487da9675b0
Luke <slukehall92@gmail.com>
2026-03-28 14:38:15 -0400

    Add Sales Pitch and Company Name to the content box

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import {contentBox} from './pageDesign.js';
+import {contentBox, restaurantName, restaurantAbout} from './pageDesign.js';
 
 console.log("I love you my gwapa prinsesa");
 // alert("Jenelyn I love you so much my love");
