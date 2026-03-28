0d46be1fdb56fcf68bf00d2ef68a91dba0b8914d
Luke <slukehall92@gmail.com>
2026-03-28 14:15:47 -0400

    Learning webpack
    Get Webpack set up and configured
    Learning to use .gitignore
    learning to use ES6 modules(import/export)

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+import {contentBox} from './pageDesign.js';
+
+console.log("I love you my gwapa prinsesa");
+// alert("Jenelyn I love you so much my love");
---
+++ src/pageDesign.js
@@ -0,0 +1,8 @@
+let contentBox = document.querySelector("#content");
+    // if(contentBox){
+    //     contentBox.style.width= "60vw";
+    //     contentBox.style.height= '60vh';
+    //     contentBox.style.border="2px solid black";
+    // }
+
+export{contentBox};
